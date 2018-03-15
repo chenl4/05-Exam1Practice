9abba1206648519e84c105de77470c5c45ef7538
chenl4 <chenl4@rose-hulman.edu>
2018-03-15 08:07:20 -0400

    blah blah

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 1, problem 2.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Lilin Chen.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -110,6 +110,21 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+
+    circle.attach_to(window)
+    rectangle.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+    if rectangle.corner_1.x > rectangle.corner_2.x:
+        start_point = rg.Point(rectangle.corner_1.x, rectangle.corner_1.y)
+        end_point = rg.Point(rectangle.corner_2.x, rectangle.corner_2.y)
+    else:
+        start_point = rg.Point(rectangle.corner_2.x, rectangle.corner_2.y)
+        end_point = rg.Point(rectangle.corner_1.x, rectangle.corner_1.y)
+    line = rg.Line(start_point, end_point)
+    line.arrow = 'last'
+    line.attach_to(window)
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
